1b0c4ee8215aba8ea06851f7faed6b8946ac1dea
cynika <tgs9915@163.com>
2022-11-27 20:13:32 +0800

    1.1

--- starter/arg_data.py
+++ starter/arg_data.py
@@ -5,7 +5,7 @@ cmpt_args = {"正常模式(无需兼容)": "", "16xx显卡兼容模式": "--prec
 GPU_args = {"0": "--skip-torch-cuda-test --precision full --no-half --use-cpu all", "1": ""}
 component_args = {"xformers优化": "--xformers", "share公开": "--share", "api接口": "--api",
                   "OutPaint(√api)": "--cors-allow-origins=https://www.painthua.com --api",
-                  "deepdanbooru": "--deepdanbooru", "UI夜间模式": "--theme=dark"}
+                  "UI明亮模式": "--theme light", "UI夜间模式": "--theme dark"}
 
 update_file = "./update_webui更新.bat"
 bat_file = "./start_webui启动.bat"
